1fc7015dfeaea5d69bbe34e10f88a7a2cb9ad169
USER <portillom@g.cofc.edu>
2019-01-03 19:11:52 -0800

    Initial commit

---
+++ app/src/main/java/com/michaelportillo/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,13 @@
+package com.michaelportillo.android.beatbox;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by USER on 1/3/19.
+ */
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance(){
+        return new BeatBoxFragment();
+    }
+}
